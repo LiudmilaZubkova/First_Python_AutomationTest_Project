f3c61f2f0240ced5da7d90ac892ea8d2ea174b04
Liudmila Zubkova <lumilli@gmail.com>
2024-01-17 22:08:30 +0100

    Custom command line parameters for changing browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,22 @@ from selenium import webdriver
 import pytest
 
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser", action="store", default="chrome", help="browser to execute tests (chrome or firefox)"
+    )
+
+
 @pytest.fixture()
-def driver():
-    print("Creating chrome driver")
-    #my_driver = webdriver.Chrome()
-    my_driver = webdriver.Firefox()
+def driver(request):
+    browser = request.config.getoption("--browser")
+    print(f"Creating {browser} driver")
+    if browser == "chrome":
+        my_driver = webdriver.Chrome()
+    elif browser == "firefox":
+        my_driver = webdriver.Firefox()
+    else:
+        raise TypeError(f"Expected 'chrome or firefox, but got {browser}")
     yield my_driver
-    print("Closing chrome diver")
+    print(f"Closing {browser} diver")
     my_driver.quit()
