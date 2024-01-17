fe2cb76f1abf1527457feaab9dc85c17ce959710
Liudmila Zubkova <lumilli@gmail.com>
2024-01-17 21:27:17 +0100

    use Fierfox for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,8 @@ import pytest
 @pytest.fixture()
 def driver():
     print("Creating chrome driver")
-    my_driver = webdriver.Chrome()
+    #my_driver = webdriver.Chrome()
+    my_driver = webdriver.Firefox()
     yield my_driver
     print("Closing chrome diver")
     my_driver.quit()
