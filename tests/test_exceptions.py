587f2073d9f27a9349734716e77acce6bd8615db
Liudmila Zubkova <lumilli@gmail.com>
2024-01-28 14:34:34 +0100

    add test_element_not_interactable_exception

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -20,4 +20,25 @@ class TestExceptions:
         # Verify Row 2 input field is displayed
         assert row_2_input_element.is_displayed(), "Row 2 input should be displayed, but it is not."
 
-
+    @pytest.mark.exceptions
+    @pytest.mark.debug
+    def test_element_not_interactable_exception(self, driver):
+        # Open page
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
+        # Click Add button
+        add_button_locator = driver.find_element(By.XPATH, "//button[@id='add_btn']")
+        add_button_locator.click()
+        # Wait for the second row to load
+        wait = WebDriverWait(driver, 10)
+        row_2_input_element = wait.until(ec.presence_of_element_located(
+            (By.XPATH, "//div[@id='rows']/div[3]/div[@class='row']/input[@type='text']")))
+        # Type text into the second input field
+        row_2_input_element.send_keys("Sushi")
+        # Push Save button using locator By.name(“Save”)
+        save_button_locator = driver.find_element(
+            By.XPATH, "//div[@id='rows']/div[3]/div[@class='row']/button[@id='save_btn']")
+        save_button_locator.click()
+        #Verify text saved
+        confirmation_element = wait.until(ec.visibility_of_element_located((By.ID, "confirmation")))
+        confirmation_message = confirmation_element.text
+        assert confirmation_message == "Row 2 was saved", "Confirmation message isn't expected"
