b07feb7b488a5e2c5c9ae95f768b7a05ea3b8d04
Liudmila Zubkova <lumilli@gmail.com>
2024-03-06 19:32:59 +0100

    correct error in test_login_page.py

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -9,16 +9,9 @@ class TestPositiveScenarios:
     @pytest.mark.positive
     def test_positive_login(self, driver):
         login_page = LoginPage(driver)
-        # Open page
         login_page.open()
-        # Type username student into Username field
-        # Type password Password123 into Password field
-        # Puch Submit button
         login_page.execute_login("student", "Password123")
         logged_in_page = LoggedInSuccessfullyPage(driver)
-        # Verify new page URL contains practicetestautomation.com/logged-in-successfully/
-        assert logged_in_page.expected_url == logged_in_page.expected_url, "Actual URL is not the same as expected"
-        # Verify new page contains expected text ('Congratulations' or 'successfully logged in')
+        assert logged_in_page.expected_url == logged_in_page.current_url, "Actual URL is not the same as expected"
         assert logged_in_page.header == "Logged In Successfully", "Header is not expected"
-        # Verify button Log out is displayed on the new page
         assert logged_in_page.is_logout_button_displayed(), "Logout button should be visible"
